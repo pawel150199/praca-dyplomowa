f4a4fe416193c42e96b643098e0e98852069cc96
pawel150199 <pawel.polski99@gmail.com>
2022-10-19 22:10:44 +0200

    add Main Execution

--- main/Experiment.py
+++ main/Experiment.py
@@ -1,8 +1,9 @@
 import sys
 from strlearn.metrics import balanced_accuracy_score, recall
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.naive_bayes import GaussianNB()
-from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC, LinearSVC
+from sklearn.tree import DecisionTreeClassifier
 sys.path.append('../ensemble')
 from Bagging import BaggingClassifier
 from RandomSamplePartition import RandomSamplePartition
@@ -17,9 +18,11 @@ sys.path.append('../evaluation')
 from Evaluator import Evaluator
 from StatisticTest import StatisticTest
 
-"""Ewaluacja eksperymentu"""
+"""Eksperyment"""
 
-def evaluation_base_kNN():
+
+def evaluation_kNN():
+    """KNN"""
     #Klasyfikatory
     clfs = {
         'Bagging' : BaggingClassifier(base_estimator=KNeighborsClassifier(), n_estimators=5),
@@ -32,77 +35,17 @@ def evaluation_base_kNN():
         'URSE' : URSE(base_estimator=KNeighborsClassifier(), n_estimators=5),
         'URSP' : URSP(base_estimator=KNeighborsClassifier(), n_estimators=5),
     }
-
     #Zbiór danych
     datasets = ['appendicitis', 'bupa']
-
     #metryki
     metrics = {
         'BAC' : balanced_accuracy_score,
         'Recall' : recall
     }
-
     ev = Evaluator(datasets=datasets, storage_dir="results", random_state=1410, metrics=metrics)
-    ev.process(clfs, 'kNN')
+    ev.process(clfs, "kNN")
     st = StatisticTest(ev)
-    st.process('kNN')
+    st.process("kNN")
 
-def evaluation_base_SVC():
-    #Klasyfikatory
-    clfs = {
-        'Bagging' : BaggingClassifier(base_estimator=SVC(), n_estimators=5),
-        'RSE' : RandomSubspaceEnsemble(base_estimator=SVC(), n_estimators=5),
-        'RSP' : RandomSamplePartition(base_estimator=SVC(), n_estimators=5),
-        'OB' : OB(base_estimator=SVC(), n_estimators=5),
-        'ORSE' : ORSE(base_estimator=SVC(), n_estimators=5),
-        'ORSP' : ORSP(base_estimator=SVC(), n_estimators=5),
-        'UB' : UB(base_estimator=SVC(), n_estimators=5),
-        'URSE' : URSE(base_estimator=SVC(), n_estimators=5),
-        'URSP' : URSP(base_estimator=SVC(), n_estimators=5),
-    }
-
-    #Zbiór danych
-    datasets = ['appendicitis', 'bupa']
-
-    #metryki
-    metrics = {
-        'BAC' : balanced_accuracy_score,
-        'Recall' : recall
-    }
-
-    ev = Evaluator(datasets=datasets, storage_dir="results", random_state=1410, metrics=metrics)
-    ev.process(clfs, 'SVC')
-    st = StatisticTest(ev)
-    st.process('SVC')
-
-def evaluation_base_SVC():
-    #Klasyfikatory
-    clfs = {
-        'Bagging' : BaggingClassifier(base_estimator=SVC(), n_estimators=5),
-        'RSE' : RandomSubspaceEnsemble(base_estimator=SVC(), n_estimators=5),
-        'RSP' : RandomSamplePartition(base_estimator=SVC(), n_estimators=5),
-        'OB' : OB(base_estimator=SVC(), n_estimators=5),
-        'ORSE' : ORSE(base_estimator=SVC(), n_estimators=5),
-        'ORSP' : ORSP(base_estimator=SVC(), n_estimators=5),
-        'UB' : UB(base_estimator=SVC(), n_estimators=5),
-        'URSE' : URSE(base_estimator=SVC(), n_estimators=5),
-        'URSP' : URSP(base_estimator=SVC(), n_estimators=5),
-    }
-
-    #Zbiór danych
-    datasets = ['appendicitis', 'bupa']
-
-    #metryki
-    metrics = {
-        'BAC' : balanced_accuracy_score,
-        'Recall' : recall
-    }
-
-    ev = Evaluator(datasets=datasets, storage_dir="results", random_state=1410, metrics=metrics)
-    ev.process(clfs, 'SVC')
-    st = StatisticTest(ev)
-    st.process('SVC')
-
-if __name__=='__main__':
-    main()
-    
+def evaluation_SVC():
+    """SVC"""
