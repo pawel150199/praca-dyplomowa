cfc0ba4f005fc9609a7ca3bf4ca0c46a144752b1
pawel150199 <pawel.polski99@gmail.com>
2022-12-10 14:45:42 +0100

    experiment 3

--- analysis/RadarGraph.py
+++ analysis/RadarGraph.py
@@ -5,7 +5,7 @@ import numpy as np
 clfs = ["Bagging", "RSM", "RSP", "OB", "ORSM", "ORSP", "UB", "URSM", "URSP"]
 metrics = ["BAC", "G-mean", "F1-score", "Precision", "Recall", "Specificity"]
 
-ranks = np.load("../results/ENSEMBLES/Ranks_KNN.npy")
+ranks = np.load("../results/ENSEMBLES-4/Ranks_CART.npy")
 mean_ranks = np.mean(ranks, axis=1)
 
 N = mean_ranks.shape[0]
@@ -18,14 +18,14 @@ ax.set_theta_direction(-1)
 ax.set_rlabel_position(0)
 ax.spines["polar"].set_visible(False)
 
-plt.xticks(angles[:-1], metrics)
-plt.yticks([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], color="grey", size=9)
+plt.xticks(angles[:-1], metrics, fontsize=12)
+plt.yticks([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], color="grey", size=10)
 plt.ylim(0,9)
-plt.title("kNN", loc="center")
+plt.title("CART", fontsize=15)
 
 # Line style
 ls = ["-", "--", "-.", ":", "-", "--", "-.", ":", "-", "--"]
-lw = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+lw = [0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9]
 
 for method_id, method in enumerate(clfs):
     values = mean_ranks[:, method_id].tolist()
@@ -33,10 +33,10 @@ for method_id, method in enumerate(clfs):
     ax.plot(angles, values, linewidth=lw[method_id], linestyle=ls[method_id], label=method)
 
 # Legend
-plt.legend(bbox_to_anchor=(1.2, -0.08), ncol=5, fontsize=9)
+plt.legend(bbox_to_anchor=(1.2, -0.08), ncol=3, fontsize=12)
 plt.tight_layout()
 
 # Save image
-plt.savefig("../images/KNNEnsmblesRadar", dpi=600, bbox_inches='tight')
+plt.savefig("../images/CARTEnsmblesRadar", dpi=600, bbox_inches='tight')
 
 
